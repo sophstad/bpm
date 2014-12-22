2e51f7b21be728d731f007850751dea80193146e
sophstad <sstadler96@verizon.net>
2014-12-21 20:36:44 -0500

    Upload files to Github

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify, render_template
+from urllib2 import urlopen
+import requests
+import json
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route("/")
+def data():
+  url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=sw1m97&api_key=dfc0d3f11ee15ab2f914558029a4896c&format=json&limit=13&period=1month"
+  response = requests.get(url).json()
+  return render_template('albums.html', api_data=response) 
+
+@app.errorhandler(404)
+def page_not_found(error):
+  return "Sorry, page not found", 404
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0")
+
+'''
+  def simplify():
+    for artist, plays in the_data['weeklyartistchart']['artist']:
+      yield artist['name'], plays
+  artists = simplify()
+'''
