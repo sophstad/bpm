6f13067d4e34b26b1f6dfcc93b96c9472a37fa6a
sophstad <sstadler96@verizon.net>
2014-12-31 16:46:01 -0500

    Add weekly top 10

--- app.py
+++ app.py
@@ -12,7 +12,13 @@ app.config["DEBUG"] = True
 def data():
   url = "http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=sw1m97&api_key=dfc0d3f11ee15ab2f914558029a4896c&format=json&limit=50&period=1month"
   api = requests.get(url).json()
-  return render_template('albums.html', artist_data=api)
+  return render_template('monthly.html', artist_data=api)
+
+@app.route("/weekly")
+def week():
+  url2 = "http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=sw1m97&api_key=dfc0d3f11ee15ab2f914558029a4896c&format=json&limit=10&period=7day"
+  api2 = requests.get(url2).json()
+  return render_template('weekly.html', artist_data_weekly=api2)
 
 @app.route("/search/<search_query>")
 def search(search_query):
@@ -24,10 +30,3 @@ def page_not_found(error):
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0")
-
-'''
-  def simplify():
-    for artist, plays in the_data['weeklyartistchart']['artist']:
-      yield artist['name'], plays
-  artists = simplify()
-'''
